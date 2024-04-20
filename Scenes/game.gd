26a69091df8d7344c0f8df9fb0fdd857de2f7f96
HolonProduction <holonproduction@gmail.com>
2024-04-20 13:49:19 +0200

    Switch warning

--- Scenes/game.gd
+++ Scenes/game.gd
@@ -23,6 +23,8 @@ class SwitchTowerDef:
 			lane2 = lanes.pick_random()
 
 
+const SwitchWarning := preload("res://Scenes/switch_warning.tscn")
+
 @onready var lanes: Array[Lane] = [
 	$Lanes/Lane1,
 	$Lanes/Lane2,
@@ -34,13 +36,49 @@ class SwitchTowerDef:
 
 var switches: Array[SwitchTowerDef] = []
 
+var switch_warning_1: Node3D
+var switch_warning_2: Node3D
+
+func __animate_warning_appear():
+	if switch_warning_1:
+		switch_warning_1.global_position = switches[0].lane1.global_position + Vector3.UP * 2
+		switch_warning_1.scale = Vector3.ZERO
+		switch_warning_1.show()
+	if switch_warning_2:
+		switch_warning_2.global_position = switches[0].lane2.global_position + Vector3.UP * 2
+		switch_warning_2.scale = Vector3.ZERO
+		switch_warning_2.show()
+		
+	var tween = get_tree().create_tween()
+	
+	if switch_warning_1:
+		tween.tween_property(switch_warning_1, "scale", Vector3.ONE, 0.1)
+	if switch_warning_2:
+		tween.parallel().tween_property(switch_warning_2, "scale", Vector3.ONE, 0.1).set_delay(0.05)
+	await tween.finished
+
+func __animate_warning_disappear():
+	var tween = get_tree().create_tween()
+	if switch_warning_1:
+		tween.tween_property(switch_warning_1, "scale", Vector3.ZERO, 0.1)
+	if switch_warning_2:
+		tween.parallel().tween_property(switch_warning_2, "scale", Vector3.ZERO, 0.1).set_delay(0.05)
+	await tween.finished
+
 func _ready():
 	randomize()
+	
+	switch_warning_1 = SwitchWarning.instantiate()
+	switch_warning_2 = SwitchWarning.instantiate()
+	add_child(switch_warning_1)
+	add_child(switch_warning_2)
 	
 	GameState.tower_switch_timer = get_tree().create_timer(GameState.rules.tower_switch_time)
 	GameState.tower_switch_timer.timeout.connect(__on_tower_switch, CONNECT_ONE_SHOT)
 	
 	switches.append(SwitchTowerDef.new(lanes))
+	
+	__animate_warning_appear()
 	
 	for lane in lanes:
 		var queue = TowerQueue.new()
@@ -62,6 +100,8 @@ func __on_tower_switch():
 	var tq1: TowerQueue
 	var tq2: TowerQueue
 	
+	__animate_warning_disappear()
+	
 	var switch = switches.pop_front()
 	
 	for tower_queue in tower_queues:
@@ -81,3 +121,6 @@ func __on_tower_switch():
 	
 	GameState.tower_switch_timer = get_tree().create_timer(GameState.rules.tower_switch_time)
 	GameState.tower_switch_timer.timeout.connect(__on_tower_switch, CONNECT_ONE_SHOT)
+	
+	await get_tree().create_timer(0.5).timeout
+	__animate_warning_appear()
