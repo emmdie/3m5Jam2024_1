e8c44a8b44af7a5378c36aab17c42518b6801f13
HolonProduction <holonproduction@gmail.com>
2024-04-20 09:32:48 +0200

    Setup game state

---
+++ Singletons/game_state.gd
@@ -0,0 +1,22 @@
+@tool
+extends GameStateBase
+
+
+# Some example variable.
+var highscore := Value.new(0)
+
+
+# Override this to change the path under which the state is saved.
+func _get_file_path():
+	return "user://game.state"
+
+
+# Override the password used to encrypt the file.
+# This password is randomly generated. You can keep it.
+func _get_password():
+	return "D9MEZ)5rOPyc§?§W?I%Gsxru2Q+4bEzx"
+
+
+# Should the state automaticaly load on startup and save on change.
+func _get_auto_save_load():
+	return true
