151e09c803777927ab7a5e82e435e710720daf01
Emmanuel Diehl <emmanuel.diehl@ifsr.de>
2024-04-20 18:00:09 +0200

    add ui input prompts for preview

--- Scenes/Ui/Ui.gd
+++ Scenes/Ui/Ui.gd
@@ -4,11 +4,13 @@ extends Control
 @onready var preview2 = $MarginContainer/UnitPreviewPanel/MarginContainer/HBoxContainer/UnitPreview2
 @onready var preview3 = $MarginContainer/UnitPreviewPanel/MarginContainer/HBoxContainer/UnitPreview3
 @onready var previews = [preview1, preview2, preview3]
-
-@export var fire_scene: PackedScene
+@export var left_icons: Array[Texture]
+@export var up_icons: Array[Texture]
+@export var right_icons: Array[Texture]
 
 func _ready() -> void:
 	GameState.unit_stash.changed.connect(update_preview)
+	set_input_icons()
 	
 func update_preview():
 	var stash_size = GameState.unit_stash.value.size()
@@ -20,3 +22,7 @@ func update_preview():
 			var unit = GameState.unit_stash.value[i].instantiate()
 			previews[i].set_unit(unit)
 		
+func set_input_icons():
+	preview1.set_icons(left_icons)
+	preview2.set_icons(up_icons)
+	preview3.set_icons(right_icons)
--- Scenes/Ui/unit_preview.gd
+++ Scenes/Ui/unit_preview.gd
@@ -2,6 +2,8 @@ extends PanelContainer
 
 @onready var unit = $SubViewportContainer/SubViewport/FireUnit
 @onready var viewport = $SubViewportContainer/SubViewport
+@onready var xbox_pic = $HBoxContainer/XboxStickTexture
+@onready var keyboard_pic = $HBoxContainer/keyboardTexture
 
 func set_unit(new_unit):
 	clear()
@@ -12,3 +14,7 @@ func clear():
 	if unit != null:
 		unit.queue_free()
 		unit = null
+
+func set_icons(icons: Array[Texture]):
+	keyboard_pic.texture = icons[0]
+	xbox_pic.texture = icons[1]
