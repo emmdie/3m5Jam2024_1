92c2ac8bf2b2d66bf497daef4b872fd40c9c8c51
Dekara VanHoc <vanhocgames@gmail.com>
2024-04-21 19:32:19 +0200

    fixes

--- gamerules.gd
+++ gamerules.gd
@@ -8,7 +8,7 @@ class_name GameRules
 @export var enemy_max_health: int = 10
 
 @export var tower_switch_time: float = 7.0
-@export var summon_streak_time: float = 3
-@export var summon_streak_busy: int = 1
+@export var summon_streak_time: float = 6
+@export var summon_streak_busy: int = 2
 
 @export var tie_player_advantage: bool = false
